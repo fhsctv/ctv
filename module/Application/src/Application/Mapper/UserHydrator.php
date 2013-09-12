549e20eaf1b37366da4e341b4fc0b56b710c3892
sumeragi <sumeragi@fantasymail.de>
2013-09-12 02:21:50 +0200

    fix problem in zfcuser modul using postgresql db adding userhydrator

---
+++ module/Application/src/Application/Mapper/UserHydrator.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Application\Mapper;
+
+use ZfcUser\Mapper\UserHydrator as ZfcUserHydrator;
+
+class UserHydrator extends ZfcUserHydrator {
+
+    public function extract($object)
+    {
+        //Preserve parent logic
+        $data = parent::extract($object);
+        //Avoid errors due to db insertion of null value
+
+        var_dump($data);
+
+        if (empty($data['user_id']))
+        {
+            unset($data['user_id']);
+        }
+        return $data;
+    }
+
+}
+
+?>
